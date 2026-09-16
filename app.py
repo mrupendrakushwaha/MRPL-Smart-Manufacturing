8d985fb134b6b5289ff092dff562809fa80180bd
Upendra Kumar Kushwaha <upendrakumarkushwaha555@gmail.com>
2026-09-16 22:18:11 +0530

    Format CSS styles for sidebar button in app.py

--- app.py
+++ app.py
@@ -437,22 +437,22 @@ st.markdown(
     }}
     
     [data-testid="stSidebarCollapsedControl"] button {
-    background: #e31e24 !important;
-    color: white !important;
-    border-radius: 10px !important;
-    width: 45px !important;
-    height: 45px !important;
+        background: #e31e24 !important;
+        color: white !important;
+        border-radius: 10px !important;
+        width: 45px !important;
+        height: 45px !important;
     }
 
     [data-testid="stSidebarCollapsedControl"] button svg {
-    display: none !important;
+        display: none !important;
     }
 
     [data-testid="stSidebarCollapsedControl"] button::after {
-    content: "☰";
-    font-size: 25px;
-    font-weight: bold;
-    color: white;
+        content: "☰";
+        font-size: 25px;
+        font-weight: bold;
+        color: white;
     }
     </style>
     """,
